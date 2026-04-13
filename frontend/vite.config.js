dc660ab9d12ac95061d33ec16635e1dbe2294587
jackjk521 <65947391+jackjk521@users.noreply.github.com>
2026-04-13 18:00:47 +0800

    Updated frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    allowedHosts: ['mirofish.jcsolutions.cloud'],
+    allowedHosts: true,
     host: '0.0.0.0',
     proxy: {
       '/api': {
